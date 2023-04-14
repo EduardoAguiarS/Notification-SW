3e11eff2f0d68b5d53a5a719cd452cce6b3ff4ca
Eduardo Aguiar <edu.pessoal@outlook.com>
2023-04-14 13:53:18 -0300

    PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-console.log('Service Worker Loaded...');
-
 self.addEventListener('push', event => {
   const body = event.data?.text() ?? 'Push Notification';
   event.waitUntil(
@@ -7,4 +5,23 @@ self.addEventListener('push', event => {
       body,
     })
   );
+});
+
+let staticCacheName = 'push-notification-v1';
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      return cache.addAll([
+        '/',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  )
 });
